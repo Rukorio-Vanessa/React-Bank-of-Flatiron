3932cdd3d8581c13eb82060a0c117641b4609229
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-18 12:45:08 +0300

    imported MainContainer to App

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import TableEntries from './TableEntries';
 import React from 'react';
+import MainContainer from './MainContainer';
 
 function App(){
   return (
     <div className="App">
-      <header className="App-header">
         <h3>
           Bank of Flatiron
         </h3>
         <h5>
           Transactions
-        </h5>
-      </header>
+        </h5>        
+        <div>
+          <MainContainer/>
+        </div>
     </div>
   )
 }
--- bank-of-flatiron/src/components/MainContainer.js
+++ bank-of-flatiron/src/components/MainContainer.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from 'react'
+import { useState, useEffect } from "react";
 import TableEntries from "./TableEntries";
 
 function MainContainer() {
     //a state to hold all the data we would fetch
-    const [table, setTable] = useState([])
+    const [tables, setTables] = useState([])
     //GET request for all the details to use as entries for the bank transactions
     useEffect(() => {
         fetch("http://localhost:3000/transactions")
           .then((r) => r.json())
-          .then((data) => setTable(data)) 
+          .then((data) => setTables(data)) 
       }, [])
 
       return(
         <div>
-        <TableEntries table={table} setTable={setTable}/>
+        <TableEntries />
         </div>
       )
 }
+
+export default MainContainer
