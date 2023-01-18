7c4a581af097abb41d4488770f0cecd236cc0486
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-18 12:14:49 +0300

    made GET request for transactions

--- bank-of-flatiron/src/components/MainContainer.js
+++ bank-of-flatiron/src/components/MainContainer.js
@@ -1,2 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TableEntries from "./TableEntries";
+
+function MainContainer() {
+    //a state to hold all the data we would fetch
+    const [table, setTable] = useState([])
+    //GET request for all the details to use as entries for the bank transactions
+    useEffect(() => {
+        fetch("http://localhost:3000/transactions")
+          .then((r) => r.json())
+          .then((data) => setTable(data)) 
+      }, [])
+
+      return(
+        <div>
+        <TableEntries table={table} setTable={setTable}/>
+        </div>
+      )
+}
